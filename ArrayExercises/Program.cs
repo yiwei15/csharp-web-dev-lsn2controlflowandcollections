4e016dd58670569aaa2cd524bdbdb9e2aec3b3fe
Yiwei Zhao <yiweizhao15@gmail.com>
2020-06-24 16:47:11 -0500

    Array and String Exercises

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ArrayExercises
 {
+    class IntArray
+    {
+        //Loop through the array and print out each value
+        public static void LogNumbers(int[] intArr)
+        {
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.WriteLine(intArr[i]);
+            }
+        }
+
+        //Use foreach loop to iterate items to print out each value
+
+        public static void LogNumbersForeachLoop(int[] intArr)
+        {
+            foreach (int i in intArr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        //Loop through, only print the odd numbers
+        public static void LogOddNumbers(int[] intArr)
+        {
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                if (intArr[i] % 2 == 1)
+                {
+                    Console.WriteLine(intArr[i]);
+                }
+            }
+        }
+
+    }
     class Program
     {
+        static int[] integers = { 1, 1, 2, 3, 5, 8 };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] integersVal = { 1, 1, 2, 3, 5, 8 };
+
+            IntArray.LogNumbers(integersVal);
+            Console.WriteLine('\n');
+            IntArray.LogNumbers(integers);
+            IntArray.LogOddNumbers(integers);
+            Console.WriteLine('\n');
+            IntArray.LogNumbersForeachLoop(integers);
+
         }
     }
 }
+
+/*Case1 Do array exercise within Main method;
+  Case2 Put array exercise into a method within program class; 
+        class Program (Main method(within Main method, call other methods), other methods)
+  Case3 Pass the array value into a method within Program class
+  Case4 Initialize the array value as a member field of Program Class.
+        (avoid creating static member variables for right now, we'd better creat variable in
+        main method) 
+  Case5/6 Define a static method in another class; array varible can be defined 
+        in either program class or main method;
+*/
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -2,11 +2,25 @@
 
 namespace StringExercises
 {
+    class StringSample
+    {
+        public static void CreatArrayWithDelimiter(string str, string delimiter)
+        {
+            string[] arr;
+            arr = str.Split(delimiter);
+            string newStr = string.Join(",", arr);
+            Console.WriteLine(newStr);
+        }
+    
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            //Console.WriteLine("Hello Strings!");
+            string str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house.I will not eat them with a mouse.";
+            string delimiter = "";
+            StringSample.CreatArrayWithDelimiter(str, delimiter); 
         }
     }
 }
